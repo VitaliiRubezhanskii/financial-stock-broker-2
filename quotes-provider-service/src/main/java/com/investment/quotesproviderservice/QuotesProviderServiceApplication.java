159f77a295879dba58749c617dc96191bf7aec48
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-11-14 14:07:19 +0200

    junk deleted

---
+++ quotes-provider-service/src/main/java/com/investment/quotesproviderservice/QuotesProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.investment.quotesproviderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesProviderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesProviderServiceApplication.class, args);
+    }
+
+}
---
+++ quotes-provider-service/src/test/java/com/investment/quotesproviderservice/QuotesProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investment.quotesproviderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesProviderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
