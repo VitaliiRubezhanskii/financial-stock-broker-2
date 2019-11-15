0bed7f38d039fcbf5a49021844b352c079830d15
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-11-15 12:30:02 +0200

    test of configs

--- account-service/src/main/java/com/investment/account/AccountApplication.java
+++ account-service/src/main/java/com/investment/account/AccountApplication.java
@@ -2,6 +2,8 @@ package com.investment.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
 public class AccountApplication {
