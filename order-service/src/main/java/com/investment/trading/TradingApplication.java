3cc3f8bd9d166654236d843a2a899f2c3e65ac18
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2019-11-16 18:12:55 +0200

    monitoring service configs added

--- order-service/src/main/java/com/investment/trading/TradingApplication.java
+++ order-service/src/main/java/com/investment/trading/TradingApplication.java
@@ -3,9 +3,10 @@ package com.investment.trading;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class TradingApplication {
 
     public static void main(String[] args) {
