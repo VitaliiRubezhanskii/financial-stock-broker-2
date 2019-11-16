5a8d4354fa26d50d992da5c308e0980a27f00224
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2019-11-16 17:23:30 +0200

    credentials git deleted

--- analytics-service/src/main/java/com/investment/analyticsservice/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/com/investment/analyticsservice/AnalyticsServiceApplication.java
@@ -2,8 +2,10 @@ package com.investment.analyticsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class AnalyticsServiceApplication {
 
     public static void main(String[] args) {
