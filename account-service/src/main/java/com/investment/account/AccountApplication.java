8266a74091b5d07867792dc91cc87ee3bb1bd302
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2019-11-16 18:29:33 +0200

    monitoring service configs added

--- account-service/src/main/java/com/investment/account/AccountApplication.java
+++ account-service/src/main/java/com/investment/account/AccountApplication.java
@@ -2,10 +2,11 @@ package com.investment.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountApplication {
 
     public static void main(String[] args) {
--- quotes-provider-service/src/main/java/com/investment/quotesproviderservice/QuotesProviderServiceApplication.java
+++ quotes-provider-service/src/main/java/com/investment/quotesproviderservice/QuotesProviderServiceApplication.java
@@ -2,8 +2,10 @@ package com.investment.quotesproviderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class QuotesProviderServiceApplication {
 
     public static void main(String[] args) {
