d2abeb5c6165cec2142f1292c91d5f983285415b
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2019-11-16 15:45:04 +0200

    credentials git deleted

--- account-service/src/main/java/com/investment/account/Controller.java
+++ account-service/src/main/java/com/investment/account/Controller.java
@@ -1,6 +1,7 @@
 package com.investment.account;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,7 @@ public class Controller {
     @Value("${prop.value}")
     public String prop;
 
+    @GetMapping(value = "/")
     public String getString(){
             return prop;
 
