ba16e20eb4be2162b3b37a53ac42aacf17bfc8c9
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2019-11-16 18:43:24 +0200

    monitoring service configs added

--- quotes-provider-service/src/test/java/com/investment/quotesproviderservice/QuotesProviderServiceApplicationTests.java
+++ quotes-provider-service/src/test/java/com/investment/quotesproviderservice/QuotesProviderServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.investment.quotesproviderservice;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuotesProviderServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
