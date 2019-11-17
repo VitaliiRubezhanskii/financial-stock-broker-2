011ea8b8e2654e32bdbe8c576abb3b606ec64a14
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2019-11-17 12:26:04 +0200

    gateway configured

---
+++ account-service/src/main/java/com/investment/account/Controller.java
@@ -0,0 +1,15 @@
+package com.investment.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+
+    @GetMapping("/")
+    public String getHello(){
+        return "Hello from Account-service through gateway";
+    }
+}
