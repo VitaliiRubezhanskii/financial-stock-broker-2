a9b9c9fbb29a04d439d880b514d1a8bc9c9624d2
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-11-18 13:01:11 +0200

    test of configs

---
+++ jenkins/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+echo "starting dockerd..."
+sudo dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --storage-driver=vfs &
+echo "starting jnlp slave..."
+exec java -jar /usr/share/jenkins/slave.jar \
+	-jnlpUrl $JENKINS_URL/computer/$JENKINS_SLAVE_NAME/slave-agent.jnlp \
+	-secret $JENKINS_SLAVE_SECRET
