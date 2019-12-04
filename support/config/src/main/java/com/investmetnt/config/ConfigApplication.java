0da8c18b720ce1fc39230ceff20eb1205138e6c6
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-04 11:56:33 +0200

    OAUTH2 service

--- support/config/src/main/java/com/investmetnt/config/ConfigApplication.java
+++ support/config/src/main/java/com/investmetnt/config/ConfigApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigApplication {
 
     public static void main(String[] args) {
