1bf0cdc522d71c6249b24a55f8114de8357494d1
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-06 10:13:34 +0200

    OAUTH2 service

---
+++ core/account-service/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sleep 2m
+java -jar account-service-0.0.1-SNAPSHOT.jar
+#java -jar tracing-0.0.1-SNAPSHOT.jar
---
+++ core/order-service/setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sleep 1m
+java -jar order-service-0.0.1-SNAPSHOT.jar
+#java -jar tracing-0.0.1-SNAPSHOT.jar
