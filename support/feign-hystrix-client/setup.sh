e365e72c1ccfba056e36086429fbfee3afd07df2
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-06 13:41:58 +0200

    OAUTH2 service

---
+++ support/feign-hystrix-client/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sleep 2m
+java -jar feign-hystrix-client-0.0.1-SNAPSHOT.jar
--- support/tracing/setup.sh
+++ support/tracing/setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+
 java -jar zipkin.jar
 #java -jar tracing-0.0.1-SNAPSHOT.jar
