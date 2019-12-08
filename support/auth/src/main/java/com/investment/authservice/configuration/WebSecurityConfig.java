02786162dcaa2c0cb3057f4f4164049c18bbf2b4
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2019-12-08 14:02:03 +0200

    auth credentials yml added

--- support/auth/src/main/java/com/investment/authservice/configuration/WebSecurityConfig.java
+++ support/auth/src/main/java/com/investment/authservice/configuration/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
                 .antMatchers("/oauth/**").permitAll()
                 .and()
                 .csrf().disable();
