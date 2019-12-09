390ee82871ee27010b069923a502abcc0f6c69ad
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-09 18:03:35 +0200

    OAUTH2 service

--- support/gateway/src/main/java/com/investment/gateway/GatewayApplication.java
+++ support/gateway/src/main/java/com/investment/gateway/GatewayApplication.java
@@ -2,7 +2,6 @@ package com.investment.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -11,7 +10,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
-@EnableOAuth2Sso
 @EnableZuulProxy
 @EnableHystrix
 public class GatewayApplication {
