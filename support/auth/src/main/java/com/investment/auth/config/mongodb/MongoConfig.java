5ed7018e30243dd1c785c40a1934869e9d386955
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-09 12:44:07 +0200

    OAUTH2 service

--- support/auth/src/main/java/com/investment/auth/config/mongodb/MongoConfig.java
+++ support/auth/src/main/java/com/investment/auth/config/mongodb/MongoConfig.java
@@ -1,10 +1,10 @@
-//package com.investment.auth.config.mongodb;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//
-//@Configuration
-//@EnableMongoRepositories(basePackages = "com.investment.auth.repository")
-//public class MongoConfig {
-//
-//}
+package com.investment.auth.config.mongodb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.investment.auth.repository")
+public class MongoConfig {
+
+}
