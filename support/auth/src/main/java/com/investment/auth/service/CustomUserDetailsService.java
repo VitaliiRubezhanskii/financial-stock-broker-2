a9d61046ce3afb83170b5c122f79b67a88fc7818
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-09 11:33:35 +0200

    OAUTH2 service

--- support/auth/src/main/java/com/investment/auth/service/CustomUserDetailsService.java
+++ support/auth/src/main/java/com/investment/auth/service/CustomUserDetailsService.java
@@ -1,10 +1,12 @@
 package com.investment.auth.service;
 
+import com.investment.auth.domain.User;
 import com.investment.auth.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import sun.awt.SunToolkit;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -17,6 +19,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User u = userRepository.findByUsername(username).get();
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found"));
     }
