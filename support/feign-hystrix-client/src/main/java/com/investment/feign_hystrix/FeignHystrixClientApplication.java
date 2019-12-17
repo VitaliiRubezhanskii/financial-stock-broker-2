285c55ae225b9480c8e76e9ade1008385dc4d0c4
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-17 14:50:56 +0200

    Kubernetes deploy added

--- support/feign-hystrix-client/src/main/java/com/investment/feign_hystrix/FeignHystrixClientApplication.java
+++ support/feign-hystrix-client/src/main/java/com/investment/feign_hystrix/FeignHystrixClientApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
+//@EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableOAuth2Sso
