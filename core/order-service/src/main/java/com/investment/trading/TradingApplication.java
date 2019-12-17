21287995a8d535c4bb296e2313ce5624167a35fe
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-17 12:07:45 +0200

    Kubernetes deploy added

--- core/order-service/src/main/java/com/investment/trading/TradingApplication.java
+++ core/order-service/src/main/java/com/investment/trading/TradingApplication.java
@@ -2,12 +2,12 @@ package com.investment.trading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 //import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 //@EnableOAuth2Sso
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class TradingApplication {
