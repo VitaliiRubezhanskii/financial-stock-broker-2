b9fd49f799ad355922bbe0db1ec68cf0c7195624
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2019-12-18 11:36:09 +0200

    Kubernetes deploy added

--- core/account-service/src/main/java/com/investment/account/AccountApplication.java
+++ core/account-service/src/main/java/com/investment/account/AccountApplication.java
@@ -3,12 +3,12 @@ package com.investment.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
-//@EnableDiscoveryClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class AccountApplication {
 
     public static void main(String[] args) {
--- support/auth/src/main/java/com/investment/auth/AuthServiceApplication.java
+++ support/auth/src/main/java/com/investment/auth/AuthServiceApplication.java
@@ -3,12 +3,13 @@ package com.investment.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
