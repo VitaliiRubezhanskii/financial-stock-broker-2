07b12e6d0f9547e394318209066587d387cbe82d
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2020-01-15 12:37:01 +0200

    kafka analytics

--- core/analytics-service/src/main/java/com/investment/analytics/AnalyticsServiceApplication.java
+++ core/analytics-service/src/main/java/com/investment/analytics/AnalyticsServiceApplication.java
@@ -1,22 +1,60 @@
 package com.investment.analytics;
 
+import com.investment.analytics.avro.Quote;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.binder.kafka.streams.annotations.KafkaStreamsProcessor;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+import java.util.Collections;
+import java.util.Map;
+
 
 @SpringBootApplication
-//@EnableEurekaClient
-//@EntityScan(
-//        basePackageClasses = { AnalyticsServiceApplication.class, Jsr310JpaConverters.class },
-//        basePackages = {"com.investment.analytics.*"}
-//)
-//@EnableAsync
+@EnableDiscoveryClient
+@EnableBinding(KafkaStreamsProcessor.class)
 public class AnalyticsServiceApplication {
+
+    private static final String STORE_NAME = "sensor-store";
 
     public static void main(String[] args) {
         SpringApplication.run(AnalyticsServiceApplication.class, args);
     }
 
+    @StreamListener("input")
+    @SendTo("output")
+    public KStream<String, Long> process(KStream<Object, Quote> input) {
+
+        final Map<String, String> serdeConfig = Collections.singletonMap(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
+
+        final SpecificAvroSerde<Quote> sensorSerde = new SpecificAvroSerde<>();
+        sensorSerde.configure(serdeConfig, false);
+
+        return input
+                .map((k, value) -> {
+                    String newKey = "v1";
+                    if (value.getId().toString().endsWith("v2")) {
+                        newKey = "v2";
+                    }
+                    return new KeyValue<>(newKey, value);
+                })
+                .groupByKey()
+                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as(STORE_NAME)
+                        .withKeySerde(Serdes.String())
+                        .withValueSerde(Serdes.Long()))
+                .toStream();
+
+    }
 }
