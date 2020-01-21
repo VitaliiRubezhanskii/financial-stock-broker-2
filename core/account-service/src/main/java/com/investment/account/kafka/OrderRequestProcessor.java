c50e9aff2ff8e54514e237cb18d14ecdad324dc5
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2020-01-21 18:34:38 +0200

    Saga impl

--- core/account-service/src/main/java/com/investment/account/kafka/OrderRequestProcessor.java
+++ core/account-service/src/main/java/com/investment/account/kafka/OrderRequestProcessor.java
@@ -3,11 +3,16 @@ package com.investment.account.kafka;
 import com.investment.account.kafka.avro.OrderRequest;
 import com.investment.account.kafka.avro.OrderResponse;
 import com.investment.account.util.AccountUtils;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.SendTo;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class OrderRequestProcessor {
@@ -18,10 +23,11 @@ public class OrderRequestProcessor {
     @SendTo("output")
     public KStream<String, OrderResponse> process(KStream<String, OrderRequest> input) {
 
-//        final Map<String, String> serdeConfig = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
-//
-//        final SpecificAvroSerde<Quote> sensorSerde = new SpecificAvroSerde<>();
-//        sensorSerde.configure(serdeConfig, false);
+        final Map<String, String> serdeConfig = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
+        final SpecificAvroSerde<OrderRequest> orderRequestSpecificAvroSerde = new SpecificAvroSerde<>();
+        final SpecificAvroSerde<OrderResponse> orderResponseSpecificAvroSerde = new SpecificAvroSerde<>();
+        orderRequestSpecificAvroSerde.configure(serdeConfig, false);
+        orderResponseSpecificAvroSerde.configure(serdeConfig, false);
 
         return input
                 .filter((key, value) -> accountUtils.checkAccount(value))
--- core/order-service/src/main/java/com/investment/trading/kafka/OrderCreationEventProcessor.java
+++ core/order-service/src/main/java/com/investment/trading/kafka/OrderCreationEventProcessor.java
@@ -2,18 +2,24 @@ package com.investment.trading.kafka;
 
 import com.investment.trading.api.repository.OrderRepository;
 import com.investment.trading.api.service.OrderService;
+import com.investment.trading.kafka.avro.OrderRequest;
 import com.investment.trading.kafka.avro.OrderResponse;
 import com.investment.trading.kafka.processors.KafkaProcessor;
 import com.investment.trading.mapper.OrderMapper;
 import com.investment.trading.model.domain.Order;
 import com.investment.trading.model.dto.OrderCreationDto;
 import com.investment.trading.utils.OrderUtils;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +33,14 @@ public class OrderCreationEventProcessor {
 
     @StreamListener(KafkaProcessor.INPUT)
     public void consumeOrderResponse(OrderResponse orderResponse) {
+        final Map<String, String> serdeConfig = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
+        final SpecificAvroSerde<OrderRequest> orderRequestSpecificAvroSerde = new SpecificAvroSerde<>();
+        final SpecificAvroSerde<com.investment.trading.kafka.avro.Order> orderSpecificAvroSerde = new SpecificAvroSerde<>();
+        final SpecificAvroSerde<OrderResponse> orderResponseSpecificAvroSerde = new SpecificAvroSerde<>();
+        orderRequestSpecificAvroSerde.configure(serdeConfig, false);
+        orderResponseSpecificAvroSerde.configure(serdeConfig, false);
+        orderSpecificAvroSerde.configure(serdeConfig, false);
+
         OrderCreationDto orderDto = OrderUtils.mapOrderRequestToOrderCreationDto(orderResponse);
        OrderCreationDto updatedDto = orderMapper.update(orderService.findOrderById(orderResponse.getId().toString()), orderDto);
         orderService.newOrder(updatedDto);
