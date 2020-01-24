6b896524dbc4160befa2bc25b23938353a71a955
VitaliyRu <vitalii.rubezhanskii@gmail.com>
2020-01-24 16:35:28 +0200

    Saga impl

--- core/order-service/src/main/java/com/investment/trading/kafka/OrderCreationEventProcessor.java
+++ core/order-service/src/main/java/com/investment/trading/kafka/OrderCreationEventProcessor.java
@@ -1,6 +1,7 @@
 package com.investment.trading.kafka;
 
 import avro.Order;
+import avro.OrderRequest;
 import avro.OrderResponse;
 import com.investment.trading.api.service.OrderService;
 import com.investment.trading.kafka.processors.KafkaProcessor;
@@ -10,9 +11,12 @@ import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
@@ -31,22 +35,41 @@ public class OrderCreationEventProcessor {
 
     private final KafkaProcessor processor;
 
-    @StreamListener(KafkaProcessor.INPUT)
-    public void consumeOrderResponse(OrderResponse orderResponse) {
+
+    @StreamListener("input0")
+    @SendTo("output1")
+    public KStream<String, Order> process(KStream<String, OrderResponse> input) {
+
         final Map<String, String> serdeConfig = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
 //        final SpecificAvroSerde<OrderRequest> orderRequestSpecificAvroSerde = new SpecificAvroSerde<>();
-//        final SpecificAvroSerde<com.investment.trading.kafka.avro.Order> orderSpecificAvroSerde = new SpecificAvroSerde<>();
         final SpecificAvroSerde<OrderResponse> orderResponseSpecificAvroSerde = new SpecificAvroSerde<>();
 //        orderRequestSpecificAvroSerde.configure(serdeConfig, false);
         orderResponseSpecificAvroSerde.configure(serdeConfig, false);
-//        orderSpecificAvroSerde.configure(serdeConfig, false);
 
-        Order order = OrderUtils.mapOrderRequestToOrderCreationDto(orderResponse);
-        processor.ordersChannel()
-                        .send(MessageBuilder.withPayload(Order.newBuilder().setId(3).build()).build());
-//        orderService.newOrder(orderDto)
-//                .map(message-> processor.ordersChannel()
-//                        .send(MessageBuilder.withPayload(payloadFromOrderEntity(message)).build()));
+        return input
+                .filter((key, value) -> true)
+                .map((k, value) ->  new KeyValue<>(k, Order.newBuilder().setId(3).build()));
 
     }
+
+
+
+//    @StreamListener(KafkaProcessor.INPUT)
+//    public void consumeOrderResponse(OrderResponse orderResponse) {
+//        final Map<String, String> serdeConfig = Collections.singletonMap(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://schema-registry:8081");
+////        final SpecificAvroSerde<OrderRequest> orderRequestSpecificAvroSerde = new SpecificAvroSerde<>();
+////        final SpecificAvroSerde<com.investment.trading.kafka.avro.Order> orderSpecificAvroSerde = new SpecificAvroSerde<>();
+//        final SpecificAvroSerde<OrderResponse> orderResponseSpecificAvroSerde = new SpecificAvroSerde<>();
+////        orderRequestSpecificAvroSerde.configure(serdeConfig, false);
+//        orderResponseSpecificAvroSerde.configure(serdeConfig, false);
+////        orderSpecificAvroSerde.configure(serdeConfig, false);
+//
+//        Order order = OrderUtils.mapOrderRequestToOrderCreationDto(orderResponse);
+//        processor.ordersChannel()
+//                        .send(MessageBuilder.withPayload(Order.newBuilder().setId(3).build()).build());
+////        orderService.newOrder(orderDto)
+////                .map(message-> processor.ordersChannel()
+////                        .send(MessageBuilder.withPayload(payloadFromOrderEntity(message)).build()));
+//
+//    }
 }
