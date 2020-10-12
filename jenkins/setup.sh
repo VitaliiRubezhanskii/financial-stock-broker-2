97fabe5915551785323daf335b6e5f8a3b0c1943
Vitalii Rubezhanskii <vitalii.rubizhanskyi@chisw.com>
2020-10-12 23:23:04 +0300

    configs for docker jenkins

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 #https://github.com/jenkinsci/docker/blob/master/README.md
-docker run -d -v `pwd`/data:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins:latest
+docker run -d \
+  -v `pwd`/data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -p 8080:8080 -p 50000:50000 jenkins:latest
