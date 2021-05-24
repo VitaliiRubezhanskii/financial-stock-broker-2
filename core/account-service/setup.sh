d0d831b19952d2d8843289d8d05e64a81d6fdf1b
vitalii <vitalii.rubezhanskii@gmail.com>
2021-05-24 15:05:45 +0300

    removed extra dependencies

--- core/account-service/setup.sh
+++ core/account-service/setup.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-sleep 2m
-java -jar account-service-0.0.1-SNAPSHOT.jar
+java -jar account-service-0.0.1-SNAPSHOT.jar -Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=:5005
