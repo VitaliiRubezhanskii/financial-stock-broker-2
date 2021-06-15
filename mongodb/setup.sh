67ee22a21e4a4cd4574837564cf9f53e6cb74787
vitalii <vitalii.rubezhanskii@gmail.com>
2021-06-15 18:47:05 +0300

    edit data seeding in mongo

--- mongodb/setup.sh
+++ mongodb/setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-if [ -f /replicated.txt ]; then
-  echo "Mongo is already set up"
-else
-  echo "Setting up mongo replication and seeding initial data..."
-  # Wait for few seconds until the mongo server is up
-  sleep 10
-  mongo mongo1:27017 replicate.js
-  echo "Replication done..."
-  # Wait for few seconds until replication takes effect
-  sleep 15
-  mongoimport --host mongo1 --db auth --collection user --type json --file /user.json --jsonArray
-  mongoimport --host mongo1 --db auth --collection authClientDetails --type json --file /authClientDetails.json --jsonArray
-  mongoimport --host mongo1 --db auth --collection mongoAccessToken --type json --file /mongoAccessToken.json --jsonArray
-  mongoimport --host mongo1 --db auth --collection mongoRefreshToken --type json --file /mongoRefreshToken.json --jsonArray
+#if [ -f /replicated.txt ]; then
+#  echo "Mongo is already set up"
+#else
+#  echo "Setting up mongo replication and seeding initial data..."
+#  # Wait for few seconds until the mongo server is up
+#  sleep 10
+#  mongo mongo1:27017 replicate.js
+#  echo "Replication done..."
+#  # Wait for few seconds until replication takes effect
+#  sleep 15
+  mongoimport --host mongodb_container --username root --password rootpassword --authenticationDatabase admin --db auth --collection user --type json --file /user.json --jsonArray
+  mongoimport --host mongodb_container --username root --password rootpassword --authenticationDatabase admin --db auth --collection authClientDetails --type json --file /authClientDetails.json --jsonArray
+  mongoimport --host mongodb_container --username root --password rootpassword --authenticationDatabase admin --db auth --collection mongoAccessToken --type json --file /mongoAccessToken.json --jsonArray
+  mongoimport --host mongodb_container --username root --password rootpassword --authenticationDatabase admin --db auth --collection mongoRefreshToken --type json --file /mongoRefreshToken.json --jsonArray
 
   echo "Seeding done..."
-  touch /replicated.txt
-fi
+#  touch /replicated.txt
+#fi
