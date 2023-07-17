08c074f2011d20339c8e30cf13fe05d509921acd
vitalii <vitalii.rubezhanskii@gmail.com>
2023-07-17 15:18:30 +0200

    setup helm charts and tf with argo cd

--- kind/main.tf
+++ kind/main.tf
@@ -1,3 +1,53 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.0.12"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "default" {
+  name = "cluster-1"
+  wait_for_ready = true
+  kind_config {
+    kind = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+    }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.23.4"
+    }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.23.4"
+    }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.23.4"
+    }
+  }
+}
+
+provider "kubectl" {
+  host = "${kind_cluster.default.endpoint}"
+  cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
+  client_certificate = "${kind_cluster.default.client_certificate}"
+  client_key = "${kind_cluster.default.client_key}"
+}
+
 data "kubectl_file_documents" "crds" {
   content = file("olm/crds.yaml")
 }
@@ -19,8 +69,18 @@ resource "kubectl_manifest" "olm_apply" {
   yaml_body = each.value
 }
 
+provider "helm" {
+  kubernetes {
+    host = "${kind_cluster.default.endpoint}"
+    cluster_ca_certificate = "${kind_cluster.default.cluster_ca_certificate}"
+    client_certificate = "${kind_cluster.default.client_certificate}"
+    client_key = "${kind_cluster.default.client_key}"
+  }
+}
+
 resource "helm_release" "argocd" {
   name  = "argocd"
+
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
